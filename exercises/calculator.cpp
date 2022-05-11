fe0353d4e719dfbd35f649e5be765c593d774b9c
samueleccz <s.cucuzza@outlook.com>
2022-05-11 18:16:22 +0200

    calculator.cpp finished

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,41 @@
   Multiplication: 8
   Division: 2
 */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+	int p;
+	int s;
+
+	cout << "inserisci due numeri" << endl;
+	cin >> p >> s;
+
+	char e = 's';
+
+	while(e=='s'){
+		char c;
+		cout << "+|-|-/|*" << endl;
+		cin >> c;
+		switch(c){
+			case '+':
+				cout << "SUM: " << p+s << endl;
+				break;
+			case '-':
+				cout << "DIFF: " <<p-s << endl;
+				break;
+			case '*':
+				cout << "PROD: " << p*s << endl;
+				break;
+			case '/':
+				cout << "DIV: " << p/s << endl;
+				break;
+			}
+		cout << "continuare? s/n" << endl;
+		cin >> e;
+	}
+}
